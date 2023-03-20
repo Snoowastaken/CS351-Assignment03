6d4b06db74c9dc115b107b02f5dc48fb63ec63b4
snoo <wm28744@gmail.com>
2023-03-20 14:54:28 -0500

    Did some stuff

--- src/Permutations.java
+++ src/Permutations.java
@@ -16,6 +16,113 @@ public class Permutations {
         return output.toString();
     }
 
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }else{
+            return n * factorial(n-1);
+        }
+    }
+
+    public static int uniquePermutations(String str){
+        List<Character> chars = new ArrayList<Character>();
+        for(char c : str.toCharArray()){
+            chars.add(c);
+        }
+        int upper = factorial(str.length());
+        int lower  = 1;
+        HashMap<Character, Integer> repetitions = new HashMap<Character, Integer>();
+        for(int i = 0; i< chars.size(); i++){
+            char key = chars.get(i);
+            if(repetitions.containsKey(key)){
+                repetitions.put(key, repetitions.get(key) + 1);
+            }else{
+                repetitions.put(key, 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> entry : repetitions.entrySet()){
+            if(entry.getValue() >= 2){
+                lower *= factorial(entry.getValue());
+            }
+        }
+        return upper/lower;
+    }
+
+    public static void normal(String str, int numTrials){
+        String[] permutations = new String[numTrials];
+        for(int i = 0; i < numTrials; i++){
+            permutations[i] = shuffle(str);
+        }
+        HashMap<String, Integer> repetitions = new HashMap<String, Integer>();
+        for(int i = 0; i< permutations.length; i++){
+            String key = permutations[i];
+            if(repetitions.containsKey(key)){
+                repetitions.put(key, repetitions.get(key) + 1);
+            }else{
+                repetitions.put(key, 1);
+            }
+        }
+        StringBuilder output = new StringBuilder();
+        int unique = 0;
+        for(Map.Entry<String, Integer> entry : repetitions.entrySet()){
+            if(entry.getValue() >= 1){
+                unique++;
+                output.append("\n");
+                output.append(entry.getKey());
+                output.append(" : ");
+                output.append(entry.getValue());
+            }
+        }
+
+        System.out.println("Base word: " + str);
+        System.out.println("Number of permutations from simulation: " + unique);
+        System.out.println("Number of unique permutations: " + uniquePermutations(str));
+        System.out.println("Number of trials: " + numTrials);
+        System.out.print(output.toString());
+
+
+
+    }
+
+    public static void include(String str, int numTrials, String includePattern){
+        String[] permutations = new String[numTrials];
+        for(int i = 0; i < numTrials; i++){
+            permutations[i] = shuffle(str);
+        }
+        for(int i = 0; i < permutations.length; i++) {
+            if (!permutations[i].contains(includePattern)) {
+                permutations[i] = null;
+            }
+        }
+        //remove null values from array
+        List<String> list = new ArrayList<String>(Arrays.asList(permutations));
+        list.removeAll(Collections.singleton(null));
+        permutations = list.toArray(new String[list.size()]);
+        int successfulTrials = permutations.length;
+        double proportion = (double)successfulTrials/numTrials;
+        HashMap<String, Integer> repetitions = new HashMap<String, Integer>();
+        for(int i = 0; i< permutations.length; i++){
+            String key = permutations[i];
+            if(repetitions.containsKey(key)){
+                repetitions.put(key, repetitions.get(key) + 1);
+            }else{
+                repetitions.put(key, 1);
+            }
+        }
+        //print number of permutations that include the pattern
+        StringBuilder output = new StringBuilder();
+        int unique = 0;
+        for(Map.Entry<String, Integer> entry : repetitions.entrySet()){
+            if(entry.getValue() >= 1){
+                unique++;
+            }
+        }
+        System.out.println("Base word: " + str);
+        System.out.println("Number of unique permutations: " + uniquePermutations(str));
+        System.out.println("Number of permutations that include " + includePattern + ": " + unique);
+        System.out.println(successfulTrials + " successes in " + numTrials + " trials; proportion: " + proportion);
+    }
+
 
     public static void main(String[] args){
         String wordPerm = "FALL";
@@ -48,60 +155,18 @@ public class Permutations {
                     break;
             }
         }
-        String[] permutations = new String[numTrials];
 
-        for(int i = 0; i < numTrials; i++){
-            permutations[i] = shuffle(wordPerm);
-        }
-        //print out all permutations
-        for(int i = 0; i < permutations.length; i++){
-            System.out.println(permutations[i]);}
+
         //check if includePattern is null, if not, make sure only permutations that include the pattern are in the array
         if(includePattern != null){
-            for(int i = 0; i < permutations.length; i++){
-                if(!permutations[i].contains(includePattern)){
-                    permutations[i] = null;
-                }
-            }
-        }
-        //check if excludePattern is null, if not, make sure only permutations that do not include the pattern are in the array
-        if(excludePattern != null){
-            for(int i = 0; i < permutations.length; i++){
-                if(permutations[i].contains(excludePattern)){
-                    permutations[i] = null;
-                }
-            }
-        }
-        //remove null values from array
-        List<String> list = new ArrayList<String>(Arrays.asList(permutations));
-        list.removeAll(Collections.singleton(null));
-        permutations = list.toArray(new String[list.size()]);
+            include(wordPerm, numTrials, includePattern);
 
-        HashMap<String, Integer> repetitions = new HashMap<String, Integer>();
-        for(int i = 0; i< permutations.length; i++){
-            String key = permutations[i];
-            if(repetitions.containsKey(key)){
-                repetitions.put(key, repetitions.get(key) + 1);
-            }else{
-                repetitions.put(key, 1);
-            }
-        }
-        StringBuilder output = new StringBuilder();
-        int unique = 0;
-        for(Map.Entry<String, Integer> entry : repetitions.entrySet()){
-            if(entry.getValue() >= 1){
-                unique++;
-                output.append("\n");
-                output.append(entry.getKey());
-                output.append(" : ");
-                output.append(entry.getValue());
-            }
+        } else {
+            normal(wordPerm, numTrials);
         }
 
-        System.out.println("Base word: " + wordPerm);
-        System.out.println("Number of unique permutations from simulation: " + unique);
-        System.out.println("Number of trials: " + numTrials);
-        System.out.print(output.toString());
+
+
 
     }
 
