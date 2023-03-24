d3564539ca528b87e9f683a1e3beaabfb0085c01
snoo <wm28744@gmail.com>
2023-03-24 13:30:56 -0500

    flags are setup

--- src/CoinFlips.java
+++ src/CoinFlips.java
@@ -4,11 +4,41 @@ public class CoinFlips {
 
 
    public static void main(String[] args){
+      int numCoinFlips = 10;
+      double probHeads = .5;
+      int numTrials = 10000;
+      int widthHisto = 20;
+      for(int i = 0; i < args.length; i++){
+         switch(args[i]){
+            case "-n":
+               //set numCoinFlips
+               numCoinFlips = Integer.parseInt(args[i+1]);
+               if(numCoinFlips > 99){
+                  System.out.println("Coin flips cannot be more than 99.");
+                  System.exit(0);
+               }
+               break;
+            case "-p":
+               //set probHeads
+               probHeads = Double.parseDouble(args[i+1]);
+               break;
+            case "-t":
+               //set numTrials
+               numTrials = Integer.parseInt(args[i+1]);
+               break;
+            case "-w":
+               //set widthHisto
+               widthHisto = Integer.parseInt(args[i+1]);
+               break;
+            default:
+               break;
+         }
+      }
+
 
 
 
 
 
    }
-
 }
